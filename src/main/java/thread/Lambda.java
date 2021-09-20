f6bbbe19b60a79efae09ba4e9bd66f3a497ec563
桑之未落 <15963685463@163.com>
2021-09-20 21:00:05 +0800

    git提交测试

--- src/main/java/thread/Lambda.java
+++ src/main/java/thread/Lambda.java
@@ -4,62 +4,59 @@ package thread;
  * @author wcl
  */
 public class Lambda {
-    /**
-     * ②：静态内部类
-     */
-    static class Like2 implements Like {
+	/**
+	 * ②：静态内部类
+	 */
+	static class Like2 implements Like {
 
-        @Override
-        public void lambda() {
-            System.out.println("②：静态内部类==》i like lambda");
-        }
-    }
+		@Override
+		public void lambda() {
+			System.out.println("②：静态内部类==》i like lambda");
+		}
+	}
 
-    public static void main(String[] args) {
-        Like like = new Like1();
-        like.lambda();
+	public static void main(String[] args) {
+		Like like = new Like1();
+		like.lambda();
 
-        like = new Like2();
-        like.lambda();
+		like = new Like2();
+		like.lambda();
 
-        // ③：局部内部类
-        /**
-         * 局部内部类
-         */
-        class Like3 implements Like {
+		// ③：局部内部类
+		class Like3 implements Like {
 
-            @Override
-            public void lambda() {
-                System.out.println("③：局部内部类==》i like lambda");
-            }
-        }
+			@Override
+			public void lambda() {
+				System.out.println("③：局部内部类==》i like lambda");
+			}
+		}
 
-        like = new Like3();
-        like.lambda();
+		like = new Like3();
+		like.lambda();
 
-        // ④：匿名内部类
-        like = new Like() {
-            @Override
-            public void lambda() {
-                System.out.println("④：匿名内部类==》i like lambda");
-            }
-        };
-        like.lambda();
+		// ④：匿名内部类
+		like = new Like() {
+			@Override
+			public void lambda() {
+				System.out.println("④：匿名内部类==》i like lambda");
+			}
+		};
+		like.lambda();
 
-        // ⑤：lambda表达式
-        like = () -> System.out.println("⑤：lambda表达式==》i like lambda");
-        like.lambda();
-    }
+		// ⑤：lambda表达式
+		like = () -> System.out.println("⑤：lambda表达式==》i like lambda");
+		like.lambda();
+	}
 }
 
 /**
  * 定义一个函数式接口
  */
 interface Like {
-    /**
-     * 打印
-     */
-    void lambda();
+	/**
+	 * 打印
+	 */
+	void lambda();
 }
 
 /**
@@ -67,9 +64,9 @@ interface Like {
  */
 class Like1 implements Like {
 
-    @Override
-    public void lambda() {
-        System.out.println("①：实现类==》i like lambda");
-    }
+	@Override
+	public void lambda() {
+		System.out.println("①：实现类==》i like lambda");
+	}
 }
 
